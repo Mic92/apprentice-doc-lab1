a3946d5bd378b8c83ceaba3134373ce8e4494811
Dominik Cermak <d.cermak@arcor.de>
2011-11-11 09:17:02 +0100

    Initialer Commit
    Signed-off-by: Dominik Cermak <d.cermak@arcor.de>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApprenticeDocLab1::Application.config.secret_token = '6dd4ce3ff3c4b2ecbc732151ea00979fbdeeee424f4b43f9c84ddea0e47cdeb545780ab21fce743a3d1f0fd7b08d4ffccab9097050bc2c183b13e79300bd6277'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ApprenticeDocLab1::Application.config.session_store :cookie_store, key: '_apprentice-doc-lab1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ApprenticeDocLab1::Application.config.session_store :active_record_store
