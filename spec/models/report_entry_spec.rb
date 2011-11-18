b04437fa514425a2c2c889f2254f2d44223c5a6e
Dominik Cermak <d.cermak@arcor.de>
2011-11-18 17:59:40 +0100

    Tests für das Model ReportEntry
    Signed-off-by: Dominik Cermak <d.cermak@arcor.de>

--- spec/models/report_entry_spec.rb
+++ spec/models/report_entry_spec.rb
@@ -1,5 +1,81 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe ReportEntry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def valid_attributes
+    {
+      :date => '2011-10-02 08:00:00',
+      :duration_in_hours => 1.5,
+      :text => 'Entry created.'
+    }
+  end
+
+  def valid_attributes_report
+    {
+      :period_start => '2011-10-01',
+      :period_end => '2011-10-31',
+    }
+  end
+
+  def valid_attributes_user
+    {
+      :name => 'Mustermann',
+      :forename => 'Max',
+      :zipcode => '01234',
+      :street => 'Musterstraße',
+      :city => 'Musterstadt',
+      :email => 'max@mustermann.de',
+      :deleted => false
+    }
+  end
+
+  before(:each) do
+    @user = User.create valid_attributes_user
+    @report = @user.reports.create valid_attributes_report
+  end
+
+  it "should create a new instance given valid attributes" do
+    @report.report_entries.create! valid_attributes
+  end
+
+  describe "report associations" do
+    before(:each) do
+      @entry = @report.report_entries.create valid_attributes
+    end
+
+    it "should have a report attribute" do
+      @entry.should respond_to(:report)
+    end
+
+    it "should have associated the right report" do
+      @entry.report_id.should eq(@report.id)
+      @entry.report.should eq(@report)
+    end
+  end
+
+  describe "validations" do
+    before(:each) do
+      @attr = valid_attributes
+    end
+
+    it "should require a report id" do
+      ReportEntry.new(@attr).should_not be_valid
+    end
+
+    it "should require a starting time" do
+      @attr.delete(:date)
+      @report.report_entries.build(@attr).should_not be_valid
+    end
+
+    it "should require a duration" do
+      @attr.delete(:duration_in_hours)
+      @report.report_entries.build(@attr).should_not be_valid
+    end
+
+    it "should require a text" do
+      @report.report_entries.build(@attr.merge(:text => "")).should_not be_valid
+      @attr.delete(:text)
+      @report.report_entries.build(@attr).should_not be_valid
+    end
+  end
 end
