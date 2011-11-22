ffd44b7b1f0374091490afd9a9a45d5d26a8091a
sascha.peukert <sascha.peukert@gmail.com>
2011-11-22 16:29:12 +0100

    Test für hashed_password Test für has_password? Methode
    Signed-off-by: sascha.peukert <sascha.peukert@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -172,4 +172,27 @@ describe User do
 
   end
   
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+	it "should set the encrypted password" do
+      @user.hashed_password.should_not be_blank
+    end
+	
+	
+	describe "method 'has_password?'" do
+	  
+	  it "should be true if the passwords are matching" do
+        @user.has_password?(@attr[:password]).should be_true
+      end    
+
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+	end
+	
+  end
+  
 end
