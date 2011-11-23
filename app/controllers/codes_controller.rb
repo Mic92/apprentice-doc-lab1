523518f5f59e8637200a53b43aabf071a2ed6905
Matti Hameister <Matti.Hameister@mailbox.tu-dresden.de>
2011-11-23 08:57:42 +0100

    Code Verwaltung ermoeglicht

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,23 +1,69 @@
 class CodesController < ApplicationController
   def index
+    @codes = Code.all
+    @title = "Code Liste"
+    respond_to do |format|
+      format.html
+    end
   end
 
   def show
-  end
-
-  def new
+    @code = Code.find(params[:id])
+    @title = "Code anzeigen"
+    respond_to do |format|
+      format.html
+    end
   end
 
   def edit
+    @code = Code.find(params[:id])
+    @title = "Code bearbeiten"
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def new
+    @code = Code.new
+    @title = "Code anlegen"
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def create
+    @code = Code.new(params[:code])
+    
+    respond_to do |format|
+      if @code.save
+        format.html { redirect_to @code, notice: 'Der Code wurde erfolgreich angelegt' }
+      else
+        format.html { render action: "new" }
+      end
+    end
+
   end
 
   def update
+    @code = Code.find(params[:id])
+
+    respond_to do |format|
+      if @code.update_attributes(params[:code])
+        format.html { redirect_to @code, notice: 'Der Code wurde erfolgreich bearbeitet' }
+      else
+        format.html { render action: "edit" }
+      end
+    end
   end
 
   def destroy
-  end
+    @code = Code.find(params[:id])
+    @code.destroy
 
+    respond_to do |format|
+      format.html { redirect_to codes_url }
+    end
+  end
 end
