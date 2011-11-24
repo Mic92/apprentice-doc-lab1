e8d8eb727744781c93eb621eeb95b6d73326a35a
Marcus Hänsch <haensch.marcus@googlemail.com>
2011-11-24 14:31:21 +0100

    Role Controller Tests mit sign_in & admin

--- spec/controllers/role_controller_spec.rb
+++ spec/controllers/role_controller_spec.rb
@@ -1,143 +1,179 @@
 require 'spec_helper'
 
 describe RoleController do
+  
+  
+  describe "method tests for signed in admin" do
+    #creating signed in user with admin right
+    before(:each) do
+    @user = User.create valid_attributes_user
+    @role_admin = Role.create valid_attributes_role
+    @role_admin.admin = true
+    @role_admin.save
+    @user.role_id = @role_admin.id
+    test_sign_in(@user)
+    
+    end  
+    
+    
+        
+    describe "GET 'new'" do   
 
-#general thinkings: 
-#                 -all actions can only be performed by an
-#                  admin user, that has to be logged in
-  
-  #creating admin/no admin role and a user
-  before(:each) do
-  @user = User.create valid_attributes_user
-  @role_no_admin = Role.create valid_attributes_role
-  @role_no_admin.admin = false
-  @role_no_admin.save
-  @role_admin = Role.create valid_attributes_role
-  @role_admin.admin = true
-  @role_admin.save
-  
-  end  
-  
-  
+        it "returns http success" do
+          get 'new'
+          response.should be_success
+        end
+        
+        it "should provide a new role record" do
+          get 'new'
+          assigns(:role).should be_new_record
+        end
       
-  describe "GET 'new'" do   
-      
-    describe "success" do
-      it "returns http success" do
-        get 'new'
+    end
+
+    describe "GET 'edit'" do
+      before(:each) do
+        @role = Role.create valid_attributes_role
+      end
+    
+      it "return http success" do
+        get 'edit', :id => @role
         response.should be_success
       end
       
-      it "should provide a new role record" do
-        get 'new'
-        assigns(:role).should be_new_record
+      it "should find the right role" do
+        get 'edit', :id => @role
+        response.should eq(@role)
       end
+
+    end
+
+    describe "POST 'create'" do
+
       
-      it "should only provide a new role record accessed by admin" do
-        @user.role_id = @role_admin.id
-        get 'new'
-        assigns(:role).should be_new_record
+      describe "success" do
         
-        @user.role_id = @role_no_admin.id
-        get 'new'
-        assings(:role).should_not be_new_record
-      end
-      
-    end  
-    
-  end
-
-  describe "GET 'edit'" do
-    before(:each) do
-      @role = Role.create valid_attributes_role
-    end
-  
-    it "return http success" do
-      get 'edit', :id => @role
-      response.should be_success
-    end
-    
-    it "should find the right role" do
-      get 'edit', :id => @role
-      response.should eq(@role)
-    end
-
-  end
-
-  describe "POST 'create'" do
-
-    
-    describe "success" do
-      
-      it "should make a new role" do
-        expect {
-          post 'create', :role => valid_attributes_role
-               }.to change {Role.count }.by(1)
-      
-      end
-      
-      it "should redirect to the main page" do
-      
-      end
-      
-      it "should only be created by admin users" do
-        @user.role_id = @role_admin
-        expect {
-          post 'create', :role => valid_attributes_role
-               }.to change {Role.count }.by(1)
+        it "should make a new role" do
+          expect {
+            post 'create', :role => valid_attributes_role
+                 }.to change {Role.count }.by(1)
+        end
         
-        @user.role_id = @role_no_admin
-        expect {
-          post 'create', :role => valid_attributes_role}.to_not change {Role.count }
-               
+        it "should redirect to the welcome page" do
+            post 'create', :role => valid_attributes_role
+            response.should redirect_to(welcome_path)
+        end
+        
+        it "should have a flash message" do
+          post 'create', :role => valid_attributes_role
+          flash[:notice].should =~ /erfolgreich/i
+        end
+
       end
-    end
-    
-    describe "failure" do
-    
-      it "should not make a new role" do
-        expect {
+      
+      describe "failure" do
+      
+        it "should not make a new role" do
+          expect {
+            post 'create', :role => nil
+                 }.not_to change {Role.count}
+        end
+        
+        it "should render the 'new' page" do
           post 'create', :role => nil
-               }.not_to change {Role.count}
+          response.should render_template('role/new')
+        end
+      end
+    end
+
+    describe "PUT 'update'" do
+      before(:each) do
+      @role = Role.create valid_attributes_role
       end
       
-      it "should render the 'new' page" do
-        post 'create', :role => nil
-        response.should render_template('role/new')
+      it "returns http success" do
+        put 'update'
+        response.should be_success
       end
+      
+      it "should find the right role" do
+        put 'update', :id => @role
+        assigns(:role).should eq(@role)
+      end
+      
+      describe "failure" do
+        it "should not change the role attributes" do
+          expect {
+            put 'update', :id => @role, :role => nil
+                 }.not_to change {Role.find(@role).updated_at }
+        end
+        
+        it "should render the 'edit' page" do
+          put 'update', :id => @role, :role => nil
+          response.should render_template('role/edit')
+        end
+      end
+      
+      describe "success" do
+        before(:each) do
+          @attributes = valid_attributes_role.merge(:export => false)
+        end
+        
+        it "should change the role attributes" do
+          expect {
+            put 'update', :id => @role, :role => @attributes
+                 }.to change {Role.find(@role).updated_at }
+          Role.find(@role).export.should eq(@attr.fetch(:export))
+        end
+        
+        it "should redirect to the welcome page" do
+          put 'update', :id => @role, :role => @attributes
+          response.should redirect_to(welcome_path)
+        end
+        
+        it "should have a flash message" do
+          put 'update', :id => @role, :role => @attributes
+          flash[:notice].should =~ /erfolgreich/i
+        end
+      end
+      
+      
     end
-  end
 
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
-      response.should be_success
-    end
-    
-    it "should only be updated by logged in users" do
-    
-    end
-    
-    it "should only be updated by admin users" do
-    
+    describe "delete 'destroy'" do
+      before(:each) do
+        @role = Role.create valid_attributes_role
+      end
+      it "returns http success" do
+        delete 'destroy'
+        response.should be_success
+      end
+      
+      it "should find the right role" do
+        delete 'destroy', :id => @role
+        assigns(:role).should eq(@role)
+      end
+      
+      it "should destroy the role" do
+        expect {
+          delete 'destroy', :id => @role
+               }.to change {Role.count}.by(-1)
+      end
+      
+      it "should redirect to the welcome page" do
+        delete 'destroy', :id => @role
+        response.should redirect_to(welcome_path)
+      end
+      
+      it "should have a flash message" do
+        delete 'destroy', :id => @role
+        flash[:notice].should =~ /erfolgreich/i
+      end
+      
+      
+      
     end
   end
-
-  describe "delete 'destroy'" do
-    it "returns http success" do
-      delete 'destroy'
-      response.should be_success
-    end
-    
-    it "should only be deleted by logged in users" do
-    
-    end
-    
-    it "should only be deleted by admin users" do
-    
-    end
-  end
-  
   describe "authentication" do
     before(:each) do
       @role = Role.create valid_role_attributes
@@ -170,8 +206,41 @@ describe RoleController do
       end
     end
     
-    # signed-in-user tests...
-
+    describe "testing signed-in users without admin right" do
+      before(:each) do
+        @user = User.create valid_attributes_user
+        @role_no_admin = Role.create valid_attributes_role
+        @role_no_admin.admin = true
+        @role_no_admin.save
+        @user.role_id = @role_no_admin.id
+        test_sign_in(@user)
+      end
+      
+      it "should deny access to 'new'" do
+        get 'new'
+        response.should redirect_to(welcome_path)
+      end
+      
+      it "should deny access to 'edit'" do
+        get 'edit', :id => @role
+        response.should redirect_to(welcome_path)
+      end
+      
+      it "should deny access to 'create'" do
+        post 'create', :role => valid_attributes_role
+        response.should redirect_to(welcome_path)
+      end
+      
+      it "should deny access to 'update'" do
+        put 'update', :id => @role, :role => valid_attributes_role
+        response.should redirect_to(welcome_path)      
+      end
+      
+      it "should deny access to 'destroy'" do
+        delete 'destroy', :id => @role
+        response.should redirect_to(welcome_path)
+      end
+    end
   end
 
 end
