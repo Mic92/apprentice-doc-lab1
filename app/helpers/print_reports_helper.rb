05a2505d1953f04704e32d27c201e028b0ed38c0
Matti Hameister <Matti.Hameister@mailbox.tu-dresden.de>
2011-11-29 16:07:57 +0100

    Templates nun wirklich nutzbar

--- app/helpers/print_reports_helper.rb
+++ app/helpers/print_reports_helper.rb
@@ -1,5 +1,57 @@
 module PrintReportsHelper
-  def handleRawCode
+
+  def init
     @displayCode = @rawCode
+    @user = @report.user
+    @reportEntries = @report.report_entries ||= []
+    @simpleFunctions = Hash.new
+    @simpleFunctions['username'] = :username
+    @simpleFunctions['userforename'] = :userforename
+    #...
+
+    @entryFunctions = Hash.new
+    @entryFunctions['text'] = :text
+    @entryFunctions['date'] = :date
+    @entryFunctions['durations_in_hours'] = :durations_in_hours
+  end
+
+  def handleRawCode
+    self.init
+    @simpleFunctions.each { |key, value|
+      @displayCode.gsub!("[v]#{key}[/v]",self.send(value))
+    }
+
+    @splitted = @displayCode.split("[e]")
+    @displayCode.clear
+    @splitted.each do |splitVal|
+      value = splitVal
+      #check if value is like entries[<number>]
+      if splitVal.match('entry[/^\[\d\]*$/]')
+        splitVal.strip!
+        #split the number between the []
+        entry = splitVal.split(/[\[\]\.]/)
+        entryNo = entry[1].to_i
+        entryValue = entry[3]
+        value = self.entry(entryNo,entryValue)
+      end
+      @displayCode << value
+    end
+  end
+
+  def username
+    @user.name ||= ''
+  end
+
+  def userforename
+    @user.forename ||= ''
+  end
+
+  def entry(number, value)
+    entry = @reportEntries[number]
+    if entry
+      entry.send(@entryFunctions[value])
+    else
+      ''
+    end
   end
 end
