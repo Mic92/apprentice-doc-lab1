dcba92b14f2282e99dd2a5205bef6b5b311efdaa
Matti Hameister <Matti.Hameister@mailbox.tu-dresden.de>
2011-11-29 13:09:13 +0100

    Controller falsch angelegt, sry

--- spec/views/print_report_controller/show.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "print_report_controller/show.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/print_reports_controller/show.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "print_reports_controller/show.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
