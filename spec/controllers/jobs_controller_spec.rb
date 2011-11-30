fe48842147119b08dc6f6b56fdc7e462215dfc97
Matti Hameister <Matti.Hameister@mailbox.tu-dresden.de>
2011-11-30 12:37:20 +0100

    kleine Tests

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,54 +1,37 @@
 require 'spec_helper'
 
 describe JobsController do
+  before(:each) do
+    @admin = User.create valid_attributes_user
+    @role = Role.create valid_attributes_role_admin
+    @role.users << @admin
+    @job = Job.create valid_attributes_job
+  end
+  
+  describe "method tests for signed in admin" do
+    before(:each) do
+      test_sign_in(@admin)
+    end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        response.should be_success
+      end
+    end
+
+    describe "GET 'show'" do
+      it "returns http success" do
+        get 'show', :id => @job
+        response.should be_success
+      end
+    end
+
+    describe "GET 'edit'" do
+      it "returns http success" do
+        get 'edit', :id => @job
+        response.should be_success
+      end
     end
   end
-
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
-    end
-  end
-
 end
