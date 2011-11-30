4fbfdd9f5851d8d95f0d2c4e78e0ab82e31c3410
Marcus Hänsch <haensch.marcus@googlemail.com>
2011-11-30 22:37:05 +0100

    User V index test

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,63 @@
 require 'spec_helper'
 
 describe "users/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @admin_role = mock_model(Role, :admin? => true)
+    @instructor_role = mock_model(Role, :check? => true, :admin? => false)
+    @apprentice_role = mock_model(Role, :check? => false, :admin? => false)
+    @apprentice = mock_model(User, :name => 'Azubi',
+                             :forename => 'One',
+                             :role  => @apprentice_role)
+    @instructor = mock_model(User, :name => 'Ausbilder',
+                             :forename => 'One',
+                             :role  => @instructor_role)
+    @admin = mock_model(User, :name => 'Admin',
+                              :forename => 'One',
+                              :role => @admin_role)
+  end
+  
+  describe "admin view" do
+    before(:each) do
+      assign(:current_user, @admin)
+    end
+    it "should display all users" do
+      render
+      rendered.should include((l @admin.name), (l @instructor.name), (l @apprentice.name))
+    end
+    it "should have a link to create a new user" do
+      render
+      rendered.should include('href="/users/new"')
+    end
+    
+    it "should have a link to deactivate users" do
+      rendered.should include("href=\"/users/#{@admin.id}\"", "href=\"/users/#{@instructor.id}\"", "href=\"/users/#{@apprentice.id}\"")
+    end
+  
+  end
+  
+  describe "instructor view" do
+    before(:each) do
+      assign(:current_user, @instructor)
+    end
+    
+    it "should display all its apprentices" do
+      render
+      rendered.should include(l @apprentice.name)
+    end
+    
+    it "should NOT display instructors or admins" do
+      render
+      rendered.should_not include((l @admin.name), (l @instructor.name))
+    end
+
+    it "should have a link to create a new apprentice" do
+      render
+      rendered.should include('href="/users/new"')
+    end
+
+    it "should have a link to deactivate apprentices" do
+      rendered.should include("href=\"/users/#{@apprentice.id}\"")
+    end
+  end
+  
 end
