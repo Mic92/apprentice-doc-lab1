2ff50eeabd6f4e4d9150193acd0a718d8b570699
Matti Hameister <Matti.Hameister@mailbox.tu-dresden.de>
2011-12-01 09:45:23 +0100

    Test eingebaut

--- spec/controllers/print_reports_controller_spec.rb
+++ spec/controllers/print_reports_controller_spec.rb
@@ -1,12 +1,29 @@
 require 'spec_helper'
 
 describe PrintReportsController do
+  before(:each) do
+    @admin = User.create valid_attributes_user
+    @ihk = Ihk.create valid_attributes_ihk
+    @code = Code.create valid_attributes_code
+    @job = Job.create valid_attributes_job
+    @template = Template.create valid_attributes_template.merge(:job_id => @job.id, :ihk_id => @ihk.id, :code_id => @code.id)
+    @admin.template = @template
+    @role = Role.create valid_attributes_role_admin.merge(:export => true, :read => true)
+    @role.users << @admin
+    @report = @admin.reports.create valid_attributes_report
+    @admin.save
+  end
+  
+  describe "method tests for signed in admin" do
+    before(:each) do
+      test_sign_in(@admin)
+    end
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+    describe "GET 'show'" do
+      it "returns http success" do
+        get 'show', :id => @report
+        response.should be_success
+      end
     end
   end
-
 end
