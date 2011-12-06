94dcd8c392f59483b08d440eb8a640a29448a6f9
sascha.peukert <sascha.peukert@gmail.com>
2011-12-06 10:41:54 +0100

    standard sessions_controller tests korrigiert

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe SessionsController do
-
+  
+  before(:each) do
+    @user = User.create valid_attributes_user
+  end
+  
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
@@ -11,15 +15,16 @@ describe SessionsController do
 
   describe "GET 'create'" do
     it "returns http success" do
-      get 'create'
-      response.should be_success
+      get 'create', :email => @user.email, :password => @user.password, :session => { :password => @user.password, :email => @user.email }
+      response.should redirect_to(welcome_path)
     end
   end
 
   describe "GET 'destroy'" do
     it "returns http success" do
-      get 'destroy'
-      response.should be_success
+      test_sign_in(@user)
+      post 'destroy'
+      response.should redirect_to(root_path)
     end
   end
 
