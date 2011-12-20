0e1bd0dc948a452b6926713ee81cd5be23aeee8a
max <s1741243@mail.zih.tu-dresden.de>
2011-12-20 19:07:36 +0100

    ReportBot.unwritten - Ausbildungsbegin wird beachtet, kleine Logickfehler behoben

--- app/controllers/report_bot_controller.rb
+++ app/controllers/report_bot_controller.rb
@@ -38,14 +38,14 @@ class ReportBotController < ApplicationController
     @month = Time.now.month
 
     @apprentices.each do |apprentice|
-      if apprentice.deleted != true #&& apprentice.trainingbegin != nil
+      if apprentice.deleted != true && apprentice.trainingbegin != nil
         i = 1
         @date_v = Time.mktime(@year, @month)
         #get commited and accepted reports
         @reports = apprentice.reports.select {|report| report.status.stype == Status.commited || report.status.stype == Status.accepted }
         @date_array = []
         #gehe durch jeden monat, der im zurückliegendem zeitraum liegt
-        while i < ReportBotController.apprentice_period_inmonths + 1  #TODO frage nach, ob man nicht über den Ausbildungsanfang hinweg ist
+        while i < ReportBotController.apprentice_period_inmonths + 1 && apprentice.trainingbegin.to_time < @date_v - 1.months
           @date_v -= 1.months
           #berechne, wie viele Arbeitstage in dem zu prüfendem Monat sind
           @daycount = workdays( @date_v, @date_v + 1.months)
@@ -56,11 +56,14 @@ class ReportBotController < ApplicationController
             # + 1.days weil der period_end Eintrag um 1 Tag abweicht, da es den Tag symbolisiert und nicht die wirkliche zeitliche Endgrenze
             if @r_start >= @date_v && @r_end <= @date_v + 1.months
               #Report ist innerhalb des Monats
-              @daycount -= workdays( @r_start, @r_end) 
-            elsif @r_start < @date_v && @r_end <= @date_v + 1.months
+              @daycount -= workdays( @r_start, @r_end)
+            elsif @r_start < @date_v && @r_end > @date_v + 1.months
+              #Report umfasst den Zeitraum
+              @daycount -= workdays( @date_v, @date_v + 1.months)
+            elsif @r_start < @date_v && @r_end > @date_v
               #Report ist teilweise am Anfang des Monats
               @daycount -= workdays( @date_v, @r_end)
-            elsif @r_start >= @date_v && @r_end > @date_v + 1.months
+            elsif @r_start < @date_v + 1.months && @r_end > @date_v + 1.months
               #Report ist teilweise am Ende des Monats
               @daycount -= workdays( @r_start, @date_v + 1.months )
             end
@@ -70,11 +73,37 @@ class ReportBotController < ApplicationController
           end
           i += 1
         end
-        #whileloop ist zu ende, email mit monaten senden, für die ein bericht fehlt
+        #whileloop ist zu ende, für den verbleibenden Zeitraum, anfangend vom Ausbildungsbeginn prüfen
+        @daycount = workdays( apprentice.trainingbegin.to_time, @date_v)
+        @reports.each do |report|
+          @r_start = report.period_start.to_time
+          @r_end = report.period_end.to_time + 1.days
+          # + 1.days weil der period_end Eintrag um 1 Tag abweicht, da es den Tag symbolisiert und nicht die wirkliche zeitliche Endgrenze
+          if @r_start >= apprentice.trainingbegin.to_time && @r_end <= @date_v
+            #Report ist innerhalb des Monats
+            @daycount -= workdays( @r_start, @r_end)
+          elsif @r_start < apprentice.trainingbegin.to_time && @r_end > @date_v
+            #Report umfasst den Zeitraum
+            @daycount -= workdays( apprentice.trainingbegin.to_time, @date_v)
+          elsif @r_start < apprentice.trainingbegin.to_time && @r_end > apprentice.trainingbegin.to_time
+            #Report ist teilweise am Anfang des Monats
+            @daycount -= workdays( apprentice.trainingbegin.to_time, @r_end)
+          elsif @r_start < @date_v && @r_end > @date_v
+            #Report ist teilweise am Ende des Monats
+            @daycount -= workdays( @r_start, @date_v )
+          end
+        end
+        if @daycount > 0
+          @date_array << [apprentice.trainingbegin.to_time.year, apprentice.trainingbegin.to_time.month]
+        end
+        #email mit monaten senden, für die ein bericht fehlt
         if @date_array != []
           @data = { :apprentice => apprentice, :date_array => @date_array }
           UserMailer.unwritten_reports_mail(@data).deliver  #TODO richtige mail schreiben
         end
+      end
+      if apprentice.deleted != true && apprentice.trainingbegin == nil
+        #TODO Mail schreiben, dass Ausbildungsbegin leer ist
       end
     end
   end
